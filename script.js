d7c818bec4bd94bb7cd859325615ba643862f6da
ura30835 <ura30835@gmail.com>
2026-08-14 12:57:28 +0300

    Add button click alert in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Находим кнопку по её ID
+const button = document.getElementById('myButton');
+
+// Добавляем действие при клике
+button.addEventListener('click', () => {
+    alert('Привет! Кнопка работает! Ты молодец! 🚀');
+});
