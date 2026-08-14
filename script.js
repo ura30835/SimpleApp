b65e71aa8db9d7195d78e55012ee3690fd364def
ura30835 <ura30835@gmail.com>
2026-08-14 13:58:51 +0300

    Update button click alert message in script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Находим кнопку по её ID
-const button = document.getElementById('myButton');
-
-// Добавляем действие при клике
-button.addEventListener('click', () => {
-    alert('Привет! Кнопка работает! Ты молодец! 🚀');
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Ого! Кнопка работает! Ты крутой!");
 });
